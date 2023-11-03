22a80131f37bc1bd551a9588c9cf2017741091e6
Anas-jaf <ansas4565@gmail.com>
2023-11-03 20:18:40 +0300

    .

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import random
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from code_for_the_tasks.daily_dairy.utils import *
 from io import BytesIO
@@ -5,38 +6,40 @@ from PIL import Image
 from api import *
 import os
 
-# Create a state for conversation
+# ضع المتغيرات هنا رجاءا
 MAX_WIDTH = 800
 MAX_HEIGHT = 800
 
-def resize_image(image, max_width, max_height):
-    width, height = image.size
-    if width > max_width or height > max_height:
-        image.thumbnail((max_width, max_height))
-    return image
+text_responses = [
+    "تم",
+    "تمام",
+    "ساسجل هذا في المذكرات",
+    "تم يا القلب",
+    "تم التسجيل في المذكرات"
+]
+
+image_responses = [
+    "اوك",
+    "تم",
+    "ممتاز",
+    "تمت اضافة الصورة"
+]
+
+document_file = './daily_diary_document/مذكرات.docx'
 
 def process_image(update, context):
-    user_id = update.message.from_user.id
-    user_name = update.message.from_user.username
-
     image = update.message.photo[-1]
     image_file = image.get_file()
-
-    insert_image_to_docx(image_file , './daily_diary.docx')
-    update.message.reply_text("Image received and saved in a DOCX file.")
+    insert_image_to_docx(image_file , document_file)
+    update.message.reply_text(random.choice(image_responses))
 
 def start(update, context):
-    update.message.reply_text("Send me a text, and I will save it to a file.")
+    update.message.reply_text("ارسل ملف و سوف احفظه في ملف")
 
 def save_text(update, context):
     user_input = update.message.text
-
-    add_content_to_existing_docx('./daily_diary.docx',user_input , write_time_heading=True)
-    # # Save the user input to a file
-    # with open("user_input.txt", "a") as file:
-    #     file.write(user_input + "\n")
-
-    update.message.reply_text("Text saved to file.")
+    add_content_to_existing_docx(document_file ,user_input , write_time_heading=True)
+    update.message.reply_text(random.choice(text_responses))
 
 def main():
     # Replace 'YOUR_BOT_TOKEN' with your actual bot token
@@ -46,8 +49,7 @@ def main():
 
     # Handle all text messages with the save_text function
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, save_text))
-    dispatcher.add_handler(MessageHandler(Filters.photo, process_image))
-    
+    dispatcher.add_handler(MessageHandler(Filters.photo, process_image))  
     updater.start_polling()
     updater.idle()
 
